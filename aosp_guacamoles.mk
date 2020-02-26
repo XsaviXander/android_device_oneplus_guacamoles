106a96f4e6f84e6d589d710c04079ebed2cda4ad
Xsavi Xander <xsavixander@gmail.com>
2020-02-25 19:22:43 -0500

    Initial Upload

---
+++ aosp_guacamoles.mk
@@ -0,0 +1,45 @@
+#
+# Copyright (C) 2019 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from guacamole device
+$(call inherit-product, device/oneplus/guacamoles/device.mk)
+
+# Inherit some common PixelExperience stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+
+# Boot Animation
+TARGET_BOOT_ANIMATION_RES := 1440
+
+# Gapps
+TARGET_GAPPS_ARCH := arm64
+TARGET_INCLUDE_STOCK_ARCORE := true
+TARGET_INCLUDE_WIFI_EXT := true
+
+# Device identifier. This must come after all inclusions.
+PRODUCT_NAME := aosp_guacamoles
+PRODUCT_DEVICE := guacamoles
+PRODUCT_BRAND := OnePlus
+PRODUCT_MODEL := OnePlus 7 Pro
+PRODUCT_MANUFACTURER := OnePlus
+
+PRODUCT_AAPT_CONFIG := xxxhdpi
+PRODUCT_AAPT_PREF_CONFIG := xxxhdpi
+PRODUCT_CHARACTERISTICS := nosdcard
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 3120
+TARGET_SCREEN_WIDTH := 1440
+
+# Build info
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_DEVICE=OP7ProNRSpr \
+    PRODUCT_NAME=OP7ProNRSpr
+
+PRODUCT_GMS_CLIENTID_BASE := android-oneplus
---
+++ vendor_props.mk
@@ -0,0 +1,15 @@
+#
+# Copyright (C) 2019 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Display
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.sf.lcd_density=560
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.vendor.build.release_type=release \
+    ro.vendor.build.real.device=true \
+    vendor.product.device=guacamoles \
+    vendor.product.manufacturer=oneplus
